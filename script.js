24db9894c5b0dc470ef2159112030b7800fa4d9c
Magnus Felinto <42441772+MagnusFelinto@users.noreply.github.com>
2021-07-06 18:59:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ let usuarios = [{"login":"kaio@gmail.com",  "senha":"kaiolindo123", "nick": "Que
 
 
 function entrar(){
-    let usuario = document.getElementsByName('Email')[0].value.toLowerCase();
+   
+    let usuario = document.getElementsByName('username')[0].value.toLowerCase();
     let senha = document.getElementsByName('password')[0].value;
     for (var u in usuarios) {
         var us = usuarios[u];
-        if (us.login === usuario && us.senha === senha) {
+        if ((us.login === usuario || nick === nick) && us.senha === senha) {
             window.location = "index.html";
             return alert("Seja bem vindo!");  
         }
@@ -16,6 +17,7 @@ function entrar(){
     alert("Dados incorretos, tente novamente.");
     return false;
 }
+/*
 function cadastro(){
     let nick  =document.getElementsByName("nick")[0].value.toLowerCase();
     let email1=document.getElementsByName("email1")[0].value.toLowerCase();
@@ -23,8 +25,12 @@ function cadastro(){
     let senha1=document.getElementsByName("senha1")[0].value.toLowerCase();
     let senha2=document.getElementsByName("senha2")[0].value.toLowerCase();
     if (email1 === email2 && senha1=== senha2){
-        
+        alert("email e senha são iguais!")
+        if(nick ===)
+        email = email1
+        senha = senha1
+        usuarios = usuarios + {"login": email,"senha": senha,"nick":nick}}
     }
 
 
-}
+}*/
